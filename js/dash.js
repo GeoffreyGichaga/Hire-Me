72ad2a73e54d3d7a10707ae57bf1432f63dca721
Geoffrey Gichaga <geoffrey.githinji@student.moringaschool.com>
2022-02-11 08:51:35 +0300

    fx:Bug- git pages deployment

--- js/dash.js
+++ js/dash.js
@@ -1,3 +1,16 @@
 function logoutButton(){
     alert("You are About to Logout");
 }
+
+
+$("form").submit(function(e) {
+    e.preventDefault();
+    var categories=$("select[name='categories']").val();
+    var categories=$("select[name='categories']").val();
+    var categories=$("select[name='categories']").val();
+    var categories=$("select[name='categories']").val();
+
+  
+
+
+});
--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ $('.carousel').carousel(
         interval:5000
     }
 )
+
+
+function openDash(){
+    window.location="./pages/dashboard.html";
+}
